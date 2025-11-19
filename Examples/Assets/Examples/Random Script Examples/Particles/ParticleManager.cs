e3b55efdc785dcc62da1e6fa7cfa3129f2fdcfdf
mydimons <mydimons.kb@gmail.com>
2025-11-18 21:25:29 -0800

    fixed up some scripts
    - cleaned up singleton example
    - fixed particle manager
    - made particle manager a singleton

--- Examples/Assets/Examples/Random Script Examples/Particles/ParticleManager.cs
+++ Examples/Assets/Examples/Random Script Examples/Particles/ParticleManager.cs
@@ -1,54 +1,58 @@
 using UnityEngine;
 
-public static class ParticleManager : MonoBehaviour
+public class ParticleManager : MonoBehaviour
 {
+    public static ParticleManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this; else Destroy(gameObject);
+    }
+
     /// <summary>
     /// Spawns a burst particle prefab at the given position
+    /// Has adjustable color or gradient, but doesn't need to be inputted
     /// </summary>
     /// <param name="particlePrefab">Particle system to spawn (Gameobject with particle system applied)</param>
     /// <param name="position">Position to spawn the particlePrefab at</param>
     /// <param name="color">Color to set the particlePrefab to</param>
     /// <param name="gradient">Gradient to set the particlePrefab to</param>
-    public static void SpawnBurstParticle(GameObject particlePrefab, Vector3 position, Color color = default, Gradient gradient = default)
+    public void SpawnBurstParticle(GameObject particlePrefab, Vector3 position, Color color = default, Gradient gradient = default)
     {
         GameObject particleInstance = Instantiate(particlePrefab, position, Quaternion.identity);
+        ParticleSystem ps = GetParticleSystem(particleInstance);
 
-        if (color != default) SetParticleSystemColor(color);
-        else if (gradient != default) SetParticleSystemGradientColor(gradient);
+        if (color != default) SetParticleSystemColor(particleInstance, color);
+        else if (gradient != default) SetParticleSystemGradientColor(particleInstance, gradient);
 
         ps.Play();
 
-        float particleLife = ps.main.duration + ps.main.startLifetime.constantMax
+        float particleLife = ps.main.duration + ps.main.startLifetime.constantMax;
         Destroy(particleInstance, particleLife);
     }
 
     private void SetParticleSystemColor(GameObject particleSystem, Color color)
     {
-        if (!particleInstance.TryGetComponent<ParticleSystem>(out var ps))
-        {
-            PrintMissingParticleSystemMessage(particleSystem.name)
-            Destroy(particleInstance);
-            return;
-        }
-
-        var main = ps.main;
+        var main = GetParticleSystem(particleSystem).main;
         main.startColor = color;
     }
 
     private void SetParticleSystemGradientColor(GameObject particleSystem, Gradient gradient)
     {
-        if (!particleInstance.TryGetComponent<ParticleSystem>(out var ps))
-        {
-            PrintMissingParticleSystemMessage(particleSystem.name);
-            Destroy(particleInstance);
-            return;
-        }
-
-        var main = ps.main;
+        var main = GetParticleSystem(particleSystem).main;
         main.startColor = gradient;
     }
 
-    private void PrintMissingParticleSystemMessage(string name) {
-        Debug.LogWarning("The \"" particleSystem.name + "\" Prefab has no ParticleSystem component!");
+    /// <summary>
+    /// Tries to get the particle system on the given object, prints a warning message if it doesn't exist and returns null
+    /// </summary>
+    private ParticleSystem GetParticleSystem(GameObject obj)
+    {
+        if (!obj.TryGetComponent<ParticleSystem>(out var ps))
+        {
+            Debug.LogWarning("The \"" + name + "\" Prefab has no ParticleSystem component!");
+            return null;
+        }
+        return ps;
     }
 }
--- Examples/Assets/Examples/Random Script Examples/SingletonExample.cs
+++ Examples/Assets/Examples/Random Script Examples/SingletonExample.cs
@@ -6,30 +6,6 @@ public class SingletonExample : MonoBehaviour
 
     private void Awake()
     {
-        // PUT SINGLETONS HERE (SINGLETONS MUST BE IN AWAKE)
-    }
-
-    /// <summary>
-    /// Holds examples of multiple singleton patterns, to be used in the Awake() function
-    /// Pick whichever one you like best! (I prefer the Basic Singleton Pattern (Compressed)!)
-    /// </summary>
-    private void SingletonPatterns()
-    {
-        #region Singleton Pattern Example 1 - Basic if/else Singleton
-
-        // Basic Singleton Pattern
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        { 
-            Destroy(gameObject);
-        }
-
-        // Basic Singleton Pattern (Compressed)
         if (Instance == null) Instance = this; else Destroy(gameObject);
-
-        #endregion
     }
 }
