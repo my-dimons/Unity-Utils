fbaaf5e89553c29abf71bfbe4ee75c4472cce837
mydimons <mydimons.kb@gmail.com>
2025-11-25 23:13:19 -0800

    docs test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,16 @@
 # Configuration file for the Sphinx documentation builder.
+
 # -- Project information
+
 project = 'Lumache'
 copyright = '2021, Graziella'
 author = 'Graziella'
+
 release = '0.1'
 version = '0.1.0'
+
 # -- General configuration
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -13,13 +18,18 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
+
 # -- Options for HTML output
+
 html_theme = 'sphinx_rtd_theme'
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
