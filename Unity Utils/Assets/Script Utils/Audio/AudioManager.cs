06d343e8d87afb50c333929813706bb0007b315f
mydimons <mydimons.kb@gmail.com>
2025-11-26 19:08:20 -0800

    Better audio value handling
    Improved how AudioManager manages different sfx values

--- Unity Utils/Assets/Script Utils/Audio/AudioManager.cs
+++ Unity Utils/Assets/Script Utils/Audio/AudioManager.cs
@@ -1,18 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
-
-
-/*
- * This is a static class, do not add it to any objects 
- *
- * TO CREATE MORE AUDIO TYPES
- * ---
- * 1. Add a new properaty to the 'AudioType' enum below
- * 2. Add a new custom volume variable like the ones below
- * 3. Add another condition to the 'CalculateVolumeBasedOnType(float volume, AudioType audioType)' switch function
- * 
- * Requires the BackgroundMusicManager.cs and SfxManager.cs scripts to work properly
- */
+using UnityEngine.Windows.Speech;
 
 namespace UnityUtils.ScriptUtils.Audio {
     /// <summary>
@@ -20,65 +9,81 @@ namespace UnityUtils.ScriptUtils.Audio {
     /// </summary>
     public static class AudioManager
     {
-        // To add more volume types, add more properties to this array and then update
+        /// Holds different audio types for volume calculations
         public enum AudioType
         {
             sfx,
             music,
-            global
+            global,
+            custom
         }
-         
-        [Header("Audio Volumes")]
 
-        [Range(0f, 1f)]
-        [Tooltip("All volume is multiplied by this value")]
-        public static float globalVolume = 1;
-
-        [Space(8)]
-
-        [Range(0f, 1f)]
-        [Tooltip("Music volume is multiplied by this value")]
-        public static float musicVolume = 1;
-
-        [Range(0f, 1f)]
-        [Tooltip("SFX volume is multiplied by this value")]
-        public static float sfxVolume = 1;
-
-        //[Space(8)]
-
-        /*
-         * Custom volumes here!
-         */
-
-        /// <summary>
-        /// Does multiplication to volume types to the get right audio levels
-        /// </summary>
-        public static float CalculateVolumeBasedOnType(float volume, AudioType audioType) => audioType switch
+        static Dictionary<AudioType, float> audioVolumes = new Dictionary<AudioType, float>()
         {
-            AudioType.sfx => MultiplyByGlobalVolume(volume) * sfxVolume,
-            AudioType.music => MultiplyByGlobalVolume(volume) * musicVolume,
-            AudioType.global => MultiplyByGlobalVolume(volume),
-            _ => MultiplyByGlobalVolume(volume),
+            { AudioType.sfx,    1f },
+            { AudioType.music,  1f },
+            { AudioType.global, 1f },
+            { AudioType.custom, 1f },
         };
 
-        private static float MultiplyByGlobalVolume(float volume) => volume * globalVolume;
+        /// <summary>
+        /// Gets the current volume level for the specified audio type.
+        /// </summary>
+        /// <returns>The volume level for the specified audio type, as a value between 0.0 (silent) and 1.0 (maximum volume).</returns>
+        public static float GetVolume(AudioType audioType)
+        {
+            return audioVolumes[audioType];
+        }
 
         /// <summary>
-        /// Calculates the effective playback duration of an audio clip after adjusting for pitch
+        /// Adjusts the volume for the specified audio type by adding the given value to its current volume.
         /// </summary>
-        /// <returns>Adjusted clip length based on pitch (clipLength / pitch)</returns>
+        public static void ModifyVolume(AudioType audioType, float volume)
+        {
+            SetVolume(audioType, GetVolume(audioType) + volume);
+        }
+
+        /// <summary>
+        /// Sets the volume level for the specified audio type to the new volume. Clamps to a range between 0-1 inclusive
+        /// </summary>
+        public static void SetVolume(AudioType audioType, float volume)
+        {
+            audioVolumes[audioType] = Mathf.Clamp01(volume);
+        }
+
+        /// <summary>
+        /// Does multiplication to volume types to the get right audio levels.
+        /// </summary>
+        /// <returns>
+        /// Proper volume level based on audio type.
+        /// </returns>
+        public static float CalculateVolumeBasedOnType(float volume, AudioType audioType) => audioType switch
+        {
+            AudioType.sfx    => MultiplyByGlobalVolume(volume) * GetVolume(AudioType.sfx),
+            AudioType.music  => MultiplyByGlobalVolume(volume) * GetVolume(AudioType.music),
+            AudioType.global => MultiplyByGlobalVolume(volume),
+            AudioType.custom => MultiplyByGlobalVolume(volume) * GetVolume(AudioType.custom),
+            _                => MultiplyByGlobalVolume(volume),
+        };
+
+        private static float MultiplyByGlobalVolume(float volume) => volume * GetVolume(AudioType.global);
+
+        /// <summary>
+        /// Calculates the effective playback duration of an audio clip after adjusting for pitch.
+        /// </summary>
+        /// <returns>Adjusted clip length based on pitch (clipLength / pitch).</returns>
         public static float CalculateClipLength(float clipLength, float pitch) => clipLength / Math.Abs(pitch);
 
         /// <summary>
-        /// Calculates the pitch adjustment factor needed to play an audio clip at a specified duration
+        /// Calculates the pitch adjustment factor needed to play an audio clip at a specified duration.
         /// </summary>
-        /// <returns>Pitch factor to achieve the desired playback time (clipLength * time)</returns>
+        /// <returns>Pitch factor to achieve the desired playback time (clipLength * time).</returns>
         public static float CalculateClipPitchWithLength(float clipLength, float time) => clipLength * time;
 
         /// <summary>
-        /// Calculates the pitch variance to add randomness to playback
+        /// Calculates the pitch variance to add randomness to playback.
         /// </summary>
-        /// <returns>Random number between 1 - pitchVariance and 1 + pitchVariance</returns>
+        /// <returns>Random number between 1 - pitchVariance and 1 + pitchVariance.</returns>
         public static float CalculatePitchVariance(float pitchVariance) => UnityEngine.Random.Range(1 - pitchVariance, 1 + pitchVariance);
     }
 }
