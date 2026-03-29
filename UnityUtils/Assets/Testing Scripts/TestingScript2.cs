8d231b215f7b7da8100fcf1870a01bf53f4d2f4c
mydimons <mydimons.kb@gmail.com>
2026-03-28 17:07:44 -0700

    removed modifiers :(

--- UnityUtils/Assets/Testing Scripts/TestingScript2.cs
+++ UnityUtils/Assets/Testing Scripts/TestingScript2.cs
@@ -3,27 +3,9 @@ using UnityEngine.InputSystem;
 using UnityUtils.ScriptUtils.Cameras;
 using UnityUtils.ScriptUtils.Objects;
 using UnityUtils.ScriptUtils.Objects.ColorFlash;
-using UnityUtils.ScriptUtils.Objects.Modifiers;
 
 public class TestingScript2 : MonoBehaviour {
   private void Start() {
-    ModifierManager<float> modifierTest = new ModifierManager<float>();
-    modifierTest.AddModifier(new ModifierData<float>(ModifierType.Flat, 1));
-    modifierTest.AddModifier(new ModifierData<float>(ModifierType.Multiply, 5));
-    modifierTest.AddModifier(new ModifierData<float>(ModifierType.Flat, 14));
-    modifierTest.AddModifier(new ModifierData<float>(ModifierType.Divide, 3));
-    modifierTest.AddModifier(new ModifierData<float>(ModifierType.Flat, 19));
-    modifierTest.PrintModifierOrder();
-    modifierTest.PrintModifiers();
-
-    modifierTest.SortModifiers();
-
-    modifierTest.PrintModifiers();
-
-    Debug.Log("value of 1: " + modifierTest.CalculateModifiers(1));
-    Debug.Log("value of 5: " + modifierTest.CalculateModifiers(5));
-
-    modifierTest.AddTemporaryModifier(new ModifierData<float>(ModifierType.Flat, 3), 1);
 
     ObjectDelays.Delay(() => CameraShake.Screenshake(intensity: 5), 3);
     ObjectDelays.Delay(() => CameraShake.Screenshake(intensity: 5), 3.2f);
--- docs/source/ScriptUtils/Objects/Objects.rst
+++ docs/source/ScriptUtils/Objects/Objects.rst
@@ -14,5 +14,4 @@ Scripts
    
    ObjectDelays
    ObjectAnimations
-   Modifiers
    ColorFlash
